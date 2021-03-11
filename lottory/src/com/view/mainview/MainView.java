fd076b34fc23243756f603280ae22b1ffd75b57a
KuanWei <KuanWei@DESKTOP-8R38C1M>
2021-03-11 13:51:18 +0800

    select lottory appearance

--- lottory/src/com/view/mainview/MainView.java
+++ lottory/src/com/view/mainview/MainView.java
@@ -16,11 +16,22 @@ import javax.swing.border.EtchedBorder;
 import java.awt.Font;
 import java.awt.SystemColor;
 import javax.swing.SwingConstants;
+import java.awt.CardLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class MainView extends JFrame {
+	private static final String Card_BigLotto = "biglotto";
+	private static final String Card_SuperLotto = "superlotto";
+	private static final String Card_Lotto539 = "lotto539";
 
 	private JPanel contentPane;
-	private JTextField textField;
+	private JList list_LottoryType;
+	private JPanel panel_card;
 
 	/**
 	 * Launch the application.
@@ -42,48 +53,109 @@ public class MainView extends JFrame {
 	 * Create the frame.
 	 */
 	public MainView() {
-		LottoryManager manager=new LottoryManager();
+		LottoryManager manager = new LottoryManager();
 		manager.init();
-		
-		
-		
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 665, 439);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
+
 		JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.NORTH);
-		
-		JList list = new JList();
-		list.setFont(new Font("新細明體", Font.PLAIN, 14));
-		list.setBorder(new EtchedBorder(EtchedBorder.RAISED, null, null));
-		list.setModel(new AbstractListModel() {
-			String[] values = new String[] {"Big Lotto", "Super Lotto", "Lotto 539"};
+
+		list_LottoryType = new JList();
+		list_LottoryType.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				switch (list_LottoryType.getSelectedIndex()) {
+				case 0:
+					((CardLayout)panel_card.getLayout()).show(panel_card, Card_BigLotto);
+					break;
+				case 1:
+					((CardLayout)panel_card.getLayout()).show(panel_card, Card_SuperLotto);
+					break;
+				case 2:
+					((CardLayout)panel_card.getLayout()).show(panel_card, Card_Lotto539);
+					break;
+				default:
+					break;
+				}
+			}
+		});
+		list_LottoryType.setFont(new Font("新細明體", Font.PLAIN, 14));
+		list_LottoryType.setBorder(new EtchedBorder(EtchedBorder.RAISED, null, null));
+		list_LottoryType.setModel(new AbstractListModel() {
+			String[] values = new String[] { "Big Lotto", "Super Lotto", "Lotto 539" };
+
 			public int getSize() {
 				return values.length;
 			}
+
 			public Object getElementAt(int index) {
 				return values[index];
 			}
 		});
-		list.setSelectedIndex(0);
-		panel.add(list);
-		
-		textField = new JTextField();
-		textField.setHorizontalAlignment(SwingConstants.CENTER);
-		textField.setFont(new Font("新細明體", Font.PLAIN, 18));
-		panel.add(textField);
-		textField.setColumns(5);
-		
-		JButton btnNewButton = new JButton("Create");
-		btnNewButton.setFont(new Font("新細明體", Font.PLAIN, 14));
-		btnNewButton.setBackground(SystemColor.controlHighlight);
-		btnNewButton.setToolTipText("create lottory");
-		panel.add(btnNewButton);
-	}
+		list_LottoryType.setSelectedIndex(0);
+		panel.add(list_LottoryType);
 
+		JButton btnNewButton = new JButton("Start");
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				
+			}
+		});
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		btnNewButton.setBackground(SystemColor.controlHighlight);
+		btnNewButton.setFont(new Font("新細明體", Font.PLAIN, 18));
+		panel.add(btnNewButton);
+
+		/*
+		 * card panel
+		 */
+		panel_card = new JPanel();
+		panel_card.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				
+			}
+		});
+		panel_card.setBorder(new EtchedBorder(EtchedBorder.RAISED, null, null));
+		contentPane.add(panel_card, BorderLayout.CENTER);
+		panel_card.setLayout(new CardLayout(0, 0));
+
+		JPanel panel_biglotto = new JPanel();
+		panel_card.add(panel_biglotto, Card_BigLotto);
+		panel_biglotto.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel = new JLabel("Big Lotto");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("新細明體", Font.PLAIN, 18));
+		panel_biglotto.add(lblNewLabel, BorderLayout.NORTH);
+
+		JPanel panel_superlotto = new JPanel();
+		panel_card.add(panel_superlotto, Card_SuperLotto);
+		panel_superlotto.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel_1 = new JLabel("Super Lotto");
+		lblNewLabel_1.setFont(new Font("新細明體", Font.PLAIN, 18));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_superlotto.add(lblNewLabel_1, BorderLayout.NORTH);
+
+		JPanel panel_lotto539 = new JPanel();
+		panel_card.add(panel_lotto539, Card_Lotto539);
+		panel_lotto539.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel_2 = new JLabel("Lotto 539");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setFont(new Font("新細明體", Font.PLAIN, 18));
+		panel_lotto539.add(lblNewLabel_2, BorderLayout.NORTH);
+	}
 }
